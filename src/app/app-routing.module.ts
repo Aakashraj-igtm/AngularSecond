c1a1fa74d3b5b00081f9841ffdbec7c76829be37
Aakashraj-igtm <saiaakashekbote@gmail.com>
2021-03-29 01:08:28 +0530

    AngularSecond

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { PagenotfoundComponentComponent } from './pagenotfound-component/pagenotfound-component.component';
+
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+const routes: Routes = [
+  {path :'',redirectTo:'/login',pathMatch:'full'},
+    {path:'login',component:LoginComponentComponent},
+    {path:'signup',component:SignupComponentComponent},
+    {path:'**',component:PagenotfoundComponentComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+import { PagenotfoundComponentComponent } from './pagenotfound-component/pagenotfound-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    LoginComponentComponent,
+    SignupComponentComponent,
+    PagenotfoundComponentComponent
   ],
   imports: [
     BrowserModule,
